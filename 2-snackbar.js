1780abfccba9cf8fed545f3a6b3542b8972a2507
TarasDemkovych <uafenomeno@gmail.com>
2025-04-06 17:23:17 +0000

    Deploying to gh-pages from @ TarasDemkovych/goit-js-hw-10@f6fdf7d39947016af65b877d990a0d7a2f44191f 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as o}from"./assets/vendor-BbbuE1sJ.js";const s=document.querySelector(".form");s.addEventListener("submit",function(i){i.preventDefault();const t=parseInt(s.elements.delay.value),n=s.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{n==="fulfilled"?e(t):m(t)},t)}).then(e=>{o.success({message:`✅ Fulfilled promise in ${e}ms`,position:"topRight",icon:""})}).catch(e=>{o.error({message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
